c5f99cbff388b0b5ce8d0fed99ba98b37fad3074
Karl <karl@radishfiction.com>
2022-11-27 19:04:50 +0900

    put access token to private server

---
+++ src/Api/PrivateServerApi.ts
@@ -0,0 +1,24 @@
+import { privateServerUrl } from "./Common";
+
+export const transferTokenToPrivateServer = async (access_token: string, id: number): Promise<boolean> => {
+  try {
+    const body = { id, access_token }
+
+    const pushTokenUrl = `${privateServerUrl}/access_token`
+
+    const httpResponse = await fetch(pushTokenUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    });
+
+    console.log("httpResponse:", httpResponse)
+
+    return httpResponse.ok
+
+  } catch (e) {
+    console.log("private server is off")
+  }
+}
